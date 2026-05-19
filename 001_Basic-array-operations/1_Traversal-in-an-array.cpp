1db69b57d71dc5f9b4dde528b3b9306f41febd46
ashwani <ashwaniatulbishnoi2900@gmail.com>
2026-05-19 09:04:48 +0530

    traversing in array

--- 001_Basic-array-operations/1_Traversal-in-an-array.cpp
+++ 001_Basic-array-operations/1_Traversal-in-an-array.cpp
@@ -1,1 +1,33 @@
-#includde
+//----------linear traversal-------------//
+
+// #include<bits/stdc++.h>
+// using namespace std;
+
+// int main()
+// {
+//     int arr[] = {1, 2, 3, 4, 5};
+//     int n = sizeof(arr) / sizeof(arr[0]);
+
+//     for(int i=0;i<n;i++)
+//     {
+//         cout<<arr[i]<<" ";
+//     }
+    
+// }
+
+//----------reverse traversal-------------//
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int arr[] = {1, 2, 3, 4, 5};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    for(int i=n-1; i >=0;i--)
+    {
+        cout<<arr[i]<<" ";
+    }
+    
+}
